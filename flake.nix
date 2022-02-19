3683fc617aa627b5662121af7734d15343355b9b
Edmund Miller <edmund.a.miller@protonmail.com>
2022-02-19 11:52:43 -0600

    build: Add nix-flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Personal Website of Edmund Miller";
+
+  outputs = { self, nixpkgs }: {
+    devShell.x86_64-linux =
+      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in pkgs.mkShell {
+        nativeBuildInputs = [
+          pkgs.nodejs-14_x
+          pkgs.nodePackages.pnpm
+        ];
+      };
+  };
+}
+
